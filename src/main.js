f137d54a0031552b9b97383679eb7afc6ac1a05e
wanxujie <wbb21@vip.qq.com>
2018-10-08 19:06:22 +0800

    1

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ import router from "./router";
 import store from "./store";
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI)
 
 Vue.use(ElementUI)
 
